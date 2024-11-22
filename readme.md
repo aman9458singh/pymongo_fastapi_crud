605c9780a1198c22233d40e15eb3d764fb224bfc
Aman singh <aman9458singh@gmail.com>
2024-11-22 20:33:28 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+# PyMongo FastAPI CRUD  
+
+## Description  
+This project is a simple CRUD (Create, Read, Update, Delete) application built with FastAPI and PyMongo. It serves as an example for handling MongoDB operations and rendering HTML templates with FastAPI.  
+
+## Features  
+- Fetch and display notes from MongoDB on the homepage.  
+- Add new notes via an HTML form.  
+- Rendered using Jinja2 templates.  
+- Static file serving.  
+
+## How to Run  
+
+### Prerequisites  
+- Python 3.10+  
+- MongoDB installed and running  
+
+### Installation  
+
+1. Clone the repository:  
+   ```bash
+   git clone <repo-url>
+   cd pymongo_fastapi_crud
+
+
+
+   pip install -r requirements.txt
---
+++ routes/note.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from models.note import Note
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from config.db import conn
+from schema.note import noteEntity, notesEntity
+from fastapi.templating import Jinja2Templates
+
+note= APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@note.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    docs = conn.fastapi.notes.find({})
+    newDocs = []
+    for doc in docs:
+        newDocs.append({
+            "id":doc["_id"],
+            "title":doc["title"],
+            "desc":doc["desc"],
+        })
+    return templates.TemplateResponse("index.html", {"request":request , "newDocs": newDocs})
+
+
+@note.post("/")
+async def create_item(request: Request):
+    form = await request.form()
+    formDict = dict(form)
+    note= conn.fastapi.notes.insert_one(formDict) 
+    return {"success":True}
